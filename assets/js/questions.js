2f9230760d091499b7592857d0650f5a064e4a7f
Eric Seader <45432901+ejseader@users.noreply.github.com>
2023-03-15 13:18:40 -0400

    initial commit

---
+++ assets/js/questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Home Test Mastery Lexicon", "Hypertext Markup Language", "Harry Tries Mimi's Lasagna", "Hyperlinks and Textile Makeup Language"],
+        answer: "Hypertext Markup Language"
+    },
+
+    {
+        title: "Which of the following is a common name of Grifola frondosa?",
+        choices: ["Hen of the Woods", "Chariot of Fire", "Griffey's Polypore", "Frondulous maximus"],
+        answer: "Hen of the Woods"
+    },
+
+    {
+        title: "This edible mushroom is prized for it's delicious flavor",
+        choices: ["Amanita bisporigera", "Galerina marginata", "Morchella escuelenta", "Gravidex hombrii"],
+        answer: "Morchella escuelenta"
+    },
+
+    {
+        title: "The scientific name for a mushroom's stem is:",
+        choices: ["Stipe", "Stripe", "Stem", "Stipple"],
+        answer: "Stipe"
+    },
+
+    {
+        title: "This mushroom is known as the Chanterelle",
+        choices: ["Helvella", "Cantharellus", "Craterellus", "Laetiporus"],
+        answer: "Cantharellus"
+    },
+
+    {
+        title: "The mushroom once dubbed 'Psilocybe weilii' is actually which fairly widespread Psilocybe?",
+        choices: ["Psilocybe heimii", "Psilocybe fanaticus", "Psilocybe semilanceata", "Psilocybe caerulescens"],
+        answer: "Psilocybe caerulescens"
+    },
+
+
+];
